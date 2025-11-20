b7e7bb250a49ac6a3eadf6a25dae8940dc332537
Alfa-alfi-nurjanah <nurjanahalfaalfi@gmail.com>
2025-11-20 21:54:15 +0700

    ya

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,30 @@
+<style>
+    footer{
+    max-width:900px;
+    margin:32px auto 24px auto;
+    display:flex;
+    justify-content:space-between;
+    gap:24px;
+    flex-wrap:wrap;
+    color:#222;
+    font-weight:600;
+    font-size:1.08rem;
+    }
+    footer div{ flex-basis:45%; min-width:220px; }
+    footer p{ margin:.2rem 0; font-weight:400; color:var(--muted); }
+</style>
+
+  <footer>
+    <div>
+      <p><strong>Hubungi Kami</strong></p>
+      <p>UPTD PPD TANJUNGPINANG</p>
+      <p>Jalan Basuki Rahmad, No.10, Tanjungpinang</p>
+      <p>(0771) 21581</p>
+    </div>
+    <div>
+      <p><strong>Jam Layanan</strong></p>
+      <p>Senin - Jumat : 08.00 - 15.00</p>
+      <p>Sabtu : 08.00 - 12.00</p>
+      <p>uptppdpinang@gmail.com</p>
+    </div>
+  </footer>
