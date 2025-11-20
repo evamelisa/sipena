0ea24e4e6f6756adb997f95746b654e3a9f3219c
evamelisa-rpl <evamelisa2007@gmail.com>
2025-11-20 20:51:24 +0700

    20/samber update

--- resources/views/samsat-bergerak.blade.php
+++ resources/views/samsat-bergerak.blade.php
@@ -3,8 +3,6 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-
   <title>Samsat Bergerak - Layanan SAMSAT</title>
 
 </head>
