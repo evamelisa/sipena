1b4dff33b918518226e26a79a7964d5b19baec3d
evamelisa-rpl <evamelisa2007@gmail.com>
2025-11-20 20:52:39 +0700

    20/samling update

--- resources/views/samsat-keliling.blade.php
+++ resources/views/samsat-keliling.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Samsat Keliling</title>
+    <title>Samsat Keliling - Layanan Samsat</title>
 </head>
 <body>
     <p>Halaman Samsat Keliling</p>
